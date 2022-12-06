2e4b7a3a5c850f92b2ae900a50a66037f154133d
Taha Elsayed <tahasakr002004@gmail.com>
2022-12-06 17:47:10 +0100

    Erstellt die Infrastruktur von der BankiGo Project

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# BankiGo
+
+### Die Idee
+
+Die Hauptidee von diesem WebApp geht es um online-banking.
+Mithilfe des Project kann man einloggen/ausloggen oder Geld einzahlen/auszahlen
+und überweisen.
+Die Project enthält freundlichen (UI) Benutzeroberfläche, die Kontostand
+oder aktulle Umsätze anzeig.
---
+++ script.js
@@ -0,0 +1,79 @@
+/****************************** BankiGo APP***************************/
+'use strict';
+// Remember , we Use array and objects because that is the most common ways
+// of organizing data in js
+// Daten
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+const account5 = {
+  owner: 'Taha Sakr',
+  movements: [500, 2000, -100, 5000, -650, -130, 70, 1100],
+  interestRate: 2, // %
+  pin: 3333,
+};
+const account6 = {
+  owner: 'Max Haberkern',
+  movements: [600, 3000, -200, 6000, -750, -230, 80, 1200],
+  interestRate: 3,
+  pin: 1234,
+};
+const accounts = [account1, account2, account3, account4, account5, account6];
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+// LECTURES in maps([]) data structures
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+console.log(...currencies);
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
